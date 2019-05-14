cc1510f34bb892cd9548e7c487194843f99f702d
Michael Wittig <michael@widdix.de>
2019-05-14 08:28:12 +0200

    serial tests (#3)

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const test = require('ava');
 const cfntest = require('@cfn-modules/test');
 
-test('defaults', async t => {
+test.serial('defaults', async t => {
   const stackName = cfntest.stackName();
   try {
     t.log(await cfntest.createStack(`${__dirname}/defaults.yml`, stackName, {}));
